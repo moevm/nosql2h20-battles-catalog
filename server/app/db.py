57856873fe1beaeb615981d6447160c591a8505c
SuperSolik <serpetr99@gmail.com>
2020-09-23 23:17:59 +0300

    docker init

--- server/app/db.py
+++ server/app/db.py
@@ -1,5 +1,6 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
 
 def setup_db():
-    return AsyncIOMotorClient()['nosql2020-battles']
+    return AsyncIOMotorClient(os.getenv('MONGODB_HOST') or 'localhost', 27017)['nosql2020-battles']
